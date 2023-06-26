052fcdb125872a7f5c76d2172af8b2bb4a668cc5
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-06-27 08:56:59 +0900

    Time: 1 ms (99.68%), Space: 46.2 MB (97.86%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        
+        dp[0] = 0;
+        
+        for(int i = 1; i < n + 1; i++) {
+            dp[i] = dp[i / 2] + i % 2; 
+        }
+        
+        return dp;
+    }
+}
