6cdb05b0072209eb02f2212c20a798fe342cc12d
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-09-08 14:04:21 +0900

    Time: 7 ms (50.78%), Space: 43.3 MB (50.97%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -4,22 +4,23 @@ class Solution {
     
     public boolean wordBreak(String s, List<String> wordDict) {
         dict = wordDict;
-        dp = new boolean[s.length()];
+        dp = new boolean[s.length() + 1];
+        dp[0] = true;
         
-        for(int i = 0; i < s.length(); i++) {
+        for(int i = 1; i <= s.length(); i++) {
             dp[i] = canSegmented(s, i);
         }
         
-        return dp[s.length() - 1];
+        return dp[s.length()];
     }
     
     boolean canSegmented(String s, int end) {
         
-        for(int i = 0; i <= end; i++) {
-            if(i != 0 && !dp[i-1])
+        for(int i = 1; i <= end; i++) {
+            if(!dp[i-1])
                 continue;
-            if(dict.contains(s.substring(i, end + 1)))
-                return i == 0 ? true : dp[i-1];
+            if(dict.contains(s.substring(i-1, end)))
+                return true;
         }
         
         return false;
