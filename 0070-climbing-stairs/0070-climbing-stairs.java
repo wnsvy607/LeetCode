d97c2ac64b1d7b00797087082d1266381abae63a
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-03-12 12:33:25 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (69.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,15 @@
 class Solution {
-    
-    int[] dp;
     public int climbStairs(int n) {
-        dp = new int[n + 2];
-        
+        int[] dp = new int[n+1];
         dp[1] = 1;
+        if(n == 1)
+            return dp[1];
         dp[2] = 2;
         
-        for(int i = 3; i < n + 1; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i = 3 ; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-            
+        
         return dp[n];
-        
-        
     }
 }
