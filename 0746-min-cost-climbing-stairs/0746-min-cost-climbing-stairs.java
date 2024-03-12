2fccb809dc2b6339b989b305115fef81bbf9d0bb
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-03-12 12:51:07 +0900

    Time: 1 ms (41.17%), Space: 43.4 MB (21.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int [n + 1];
+        int len = cost.length;
+        int[] dp = new int[len];
         dp[0] = cost[0];
+        if(len == 1)
+            return dp[0];
         dp[1] = cost[1];
         
-        for(int i = 2; i < n; i++) {
+        
+        for(int i = 2; i < len; i++) {
             dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
         }
         
-        dp[n] = Math.min(dp[n-1], dp[n-2]);
-        
-        return dp[n];
+        return Math.min(dp[len-1], dp[len-2]);
     }
 }
