3d67a360a3544713aaf365a35a60aff87664470c
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-03-20 22:33:25 +0900

    Time: 8 ms (43.44%), Space: 43.9 MB (56.37%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,28 +1,27 @@
-class Solution {
-    boolean[] dp;
-    List<String> dict;
-    
-    public boolean wordBreak(String s, List<String> wordDict) {
-        dict = wordDict;
-        dp = new boolean[s.length() + 1];
-        dp[0] = true;
+import java.util.*;
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> list) {
+        // f[i]의 값은 i전 까지의 결과가 전부 dict에 포함된 단어인지 여부
+        boolean[] f = new boolean[s.length() + 1];
         
-        for(int i = 1; i <= s.length(); i++) {
-            dp[i] = canSegmented(s, i);
+        Set<String> dict = new HashSet<>(list);
+                
+        f[0] = true;
+        
+
+        
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                //f[j]: j 이전까지의 문자열은 모두 dict로 이루어졌는가 
+                //뒤에는 (j ~ i - 1) 서브스트링이 dict에 있는지 체크
+                if(f[j] && dict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
         }
         
-        return dp[s.length()];
-    }
-    
-    boolean canSegmented(String s, int end) {
-        
-        for(int i = 1; i <= end; i++) {
-            if(!dp[i-1])
-                continue;
-            if(dict.contains(s.substring(i-1, end)))
-                return true;
-        }
-        
-        return false;
+        return f[s.length()];
     }
 }
