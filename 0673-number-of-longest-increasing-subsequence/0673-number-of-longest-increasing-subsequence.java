f78cb2228ba487ad438ac19e278249485ad1e28d
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-05-25 13:19:57 +0900

    Time: 20 ms (51.62%), Space: 44.5 MB (10.69%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,21 +1,20 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int[] cnt = new int[nums.length];
+        int[][] dp = new int[nums.length][2];
         
         int max = 1;
         
         for(int i = 0; i < nums.length; i++) {
-            dp[i] = 1;
-            cnt[i] = 1;
+            dp[i][0] = 1;
+            dp[i][1] = 1;
             for(int j = 0; j < i; j++) {
                 if(nums[j] < nums[i]) {
-                    if(dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
-                        cnt[i] = cnt[j];
-                        max = Math.max(max, dp[i]);
-                    } else if(dp[j] + 1 == dp[i]) {
-                        cnt[i] += cnt[j];
+                    if(dp[j][0] + 1 > dp[i][0]) {
+                        dp[i][0] = dp[j][0] + 1;
+                        dp[i][1] = dp[j][1];
+                        max = Math.max(max, dp[i][0]);
+                    } else if(dp[j][0] + 1 == dp[i][0]) {
+                        dp[i][1] += dp[j][1];
                     }
                 }
                     
@@ -24,7 +23,7 @@ class Solution {
 
         int count = 0;
         for(int i = 0; i < nums.length; i++) {
-            if(dp[i] == max) count += cnt[i];
+            if(dp[i][0] == max) count += dp[i][1];
         }
         
         return count;
